ef15e03e89c6be3ce3a32cee4bdc8b21cde15525
Viktor Skorjanc <viktor.skorjanc@gmail.com>
2022-02-16 21:57:16 +0100

    Fixed PL calib import

--- bix_analysis_libraries/pl/pl_data_prep.py
+++ bix_analysis_libraries/pl/pl_data_prep.py
@@ -2,22 +2,21 @@ import scipy.constants as phys
 from bric_analysis_libraries.pl import pl_data_prep as pldp
 
 
-def calc_photon_flux(df, calib, offset, area):
+def calc_photon_flux(df, calib, area):
     '''
     Calculates photon flux [photons/(m2 s nm)] from counts [1/(s nm)].
     :param df: DataFrame indexed by wavelength
-    :param calib: Calibration curve
-    :param offset: Dark offset curve
+    :param calib: DataFrame w/ calibration and dark offset curves.  
     :param area: Spot size in m2
     :returns: Dataframe with calculated 
     '''
     def calc(x):
-        return (x - offset) * calib / area
+        return (x - calib.offset) * calib.calib / area
 
     # remove first values if the legths don't coincide
-    len_diff = len(df) - len(offset)
-    if len_diff > 0:
-        df = df.iloc[len_diff:]
+    # len_diff = len(df) - len(offset)
+    # if len_diff > 0:
+    #     df = df.iloc[len_diff:]
 
     return df.apply(calc)
 
