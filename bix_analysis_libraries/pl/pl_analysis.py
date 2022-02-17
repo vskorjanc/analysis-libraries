1e45c6f1f1339c230c36fa37e2054dfa4e4b7913
Viktor Skorjanc <viktor.skorjanc@gmail.com>
2022-02-17 18:09:22 +0100

    Implemented hetf plot

--- bix_analysis_libraries/pl/pl_analysis.py
+++ bix_analysis_libraries/pl/pl_analysis.py
@@ -1,7 +1,14 @@
 import scipy.constants as phys
 import numpy as np
+from plotly import express as px
+
 from bric_analysis_libraries import standard_functions as std
 from .. import bix_standard_functions as bsf
+
+
+def lin_reg(e, t, qfls):
+    kb = phys.physical_constants['Boltzmann constant in eV/K'][0]
+    return - e / (kb * t) + qfls / (kb * t)
 
 
 def fit_lin(df, **kwargs):
@@ -11,12 +18,18 @@ def fit_lin(df, **kwargs):
     and scipy.optimize.curve_fit().
     :returns: DataFrame with the fit parameter and error for each parameter.    
     '''
-    def lin_reg(e, t, qfls):
-        kb = phys.physical_constants['Boltzmann constant in eV/K'][0]
-        return - e / (kb * t) + qfls / (kb * t)
-
     fit = std.df_fit_function(lin_reg, **kwargs)
     return fit(df)
+
+
+def calc_log(data):
+    def calc(x):
+        h = phys.physical_constants['Planck constant in eV/Hz'][0]
+        const = (h ** 3) * (phys.c ** 2) / (2 * phys.pi)
+        return np.log(x * const / (x.index.values ** 2))
+
+    data = data.apply(calc)
+    return data.replace([-np.inf, np.inf], np.nan).dropna(how='all')
 
 
 def high_energy_tail_fit(data, window, start=None, end=None, temperature=300, temp_window=1):
@@ -31,14 +44,7 @@ def high_energy_tail_fit(data, window, start=None, end=None, temperature=300, te
     :param temp_window: Window for determining temperature bounds when. [Default: 1]
     :returns: DataFrame with parameters qfls and t and error for each parameter.
     '''
-
-    def calc(x):
-        h = phys.physical_constants['Planck constant in eV/Hz'][0]
-        const = (h ** 3) * (phys.c ** 2) / (2 * phys.pi)
-        return np.log(x * const / (x.index.values ** 2))
-
-    data = data.apply(calc)
-    data = data.replace([-np.inf, np.inf], np.nan).dropna(how='all')
+    data = calc_log(data)
 
     # def find inflection as separate?
     svg_data = bsf.apply_savgol(data, deriv=1)
@@ -48,6 +54,22 @@ def high_energy_tail_fit(data, window, start=None, end=None, temperature=300, te
     guess = (temperature, 1)
     bounds = [[temperature - temp_window, 0], [temperature + temp_window, 1.5]]
     return fit_lin(fit_data, guess=guess, bounds=bounds)
+
+
+def plot_hetf(data, fit, start=None, end=None):
+    '''
+    Plots log of flux and linear fit for visual inspection.
+    :param data: DataFrame with flux of a single measurement.
+    :param fit: DataFrame given by `high_energy_tail_fit` function.
+    :param start: Lower bound of the search range, or None. [Default: None]
+    :param end: Upper bound of the search range, or None. [Default: None]
+    :returns: Plotly figure.
+    '''
+    pl_data = calc_log(data.loc[start: end])
+    pl_data.columns = ['log']
+    pl_data['fit'] = lin_reg(pl_data.index, fit.t.value[0], fit.qfls.value[0])
+    fig = px.line(pl_data)
+    return fig
 
 
 def calculate_plqy(df, ref, emission_range, excitation_range):
