a1582c18e0b32366dba3c7703cbe103743ff6da2
Viktor Skorjanc <viktor.skorjanc@gmail.com>
2022-06-23 15:46:36 +0200

    docs: updated setup files

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
 import setuptools
 
-with open( 'README.md', 'r' ) as f:
-	long_desc = f.read()
+__version__ = '0.0.3'
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
 
 setuptools.setup(
-	name='bix_analysis_libraries',
-	version = '0.0.3',
-	author='Viktor Skorjanc',
-	author_email = 'viktor.skorjanc@gmail.com',
-	description = 'An assortment of analysis libraries.',
-	long_description = long_desc,
-	long_description_content_type = 'text/markdown',
-	url = 'https://github.com/vskorjanc/analysis-libraries',
-	packages = setuptools.find_packages(),
-	classifiers = [
-		"Programming Language :: Python :: 3",
+    name='bix_analysis_libraries',
+    version=__version__,
+    author='Viktor Skorjanc',
+    author_email='viktor.skorjanc@gmail.com',
+    description='An assortment of analysis libraries.',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vskorjanc/analysis-libraries',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-	],
-	install_requires = [
-		'bric_analysis_libraries',
-		'pandas',
-		'numpy'
-	]
+    ],
+    install_requires=[
+        'bric_analysis_libraries',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'plotly',
+        'thot-data',
+        'thot-cli'
+    ]
 )
