c76e44bfeb31b8ff88faa9f6354a39a142ca1203
Viktor Skorjanc <viktor.skorjanc@helmholtz-berlin.de>
2024-03-05 17:21:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
-__version__ = '0.1.1'
+__version__ = "0.1.1"
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_desc = f.read()
 
 setuptools.setup(
-    name='bix_analysis_libraries',
+    name="bix_analysis_libraries",
     version=__version__,
-    author='Viktor Skorjanc',
-    author_email='viktor.skorjanc@gmail.com',
-    description='An assortment of analysis libraries.',
+    author="Viktor Skorjanc",
+    author_email="viktor.skorjanc@gmail.com",
+    description="An assortment of analysis libraries.",
     long_description=long_desc,
-    long_description_content_type='text/markdown',
-    url='https://github.com/vskorjanc/analysis-libraries',
+    long_description_content_type="text/markdown",
+    url="https://github.com/vskorjanc/analysis-libraries",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,15 +21,15 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'bric_analysis_libraries==0.0.12',
-        'pandas==1.4.1',
-        'numpy==1.21.2',
-        'matplotlib==3.4.3',
-        'scipy==1.7.2',
-        'plotly==5.11.0',
-        'thot-data==0.6.3',
-        'thot-cli==0.5.1',
-        'tabulate==0.9.0',
-        'BaselineRemoval==0.1.1'
-    ]
+        "bric_analysis_libraries==0.0.12",
+        "pandas>=2.2.1",
+        "numpy>=1.26.4",
+        "matplotlib>=3.4.3",
+        "scipy>=1.12.0",
+        "plotly==5.11.0",
+        "thot-data==0.6.3",
+        "thot-cli==0.5.1",
+        "tabulate>=0.9.0",
+        "BaselineRemoval>=0.1.1",
+    ],
 )
